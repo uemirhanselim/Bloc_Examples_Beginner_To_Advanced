412964cc5b690f1fadbffd8b43f6954a0c17cad5
uemirhanselim <uemirhanselim@gmail.com>
2022-05-13 15:07:35 +0300

    A problem I couldnt figured out

--- lib/timer_app_bloc/presentation/actions.dart
+++ lib/timer_app_bloc/presentation/actions.dart
@@ -8,7 +8,9 @@ class Actionsa extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TimerBloc, TimerState>(
-      buildWhen: (prev, state) => prev.runtimeType != state.runtimeType,
+      buildWhen: (prev, state) {
+        return prev.runtimeType != state.runtimeType;
+      },
       builder: (context, state) {
         return Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
